f7ca30c2a686dfba4a1c15195564687fe5a8dc9a
johnsonxia488-ai <johnsonxia488@gmail.com>
2026-09-14 13:21:18 -0700

    Add files via upload

---
+++ data.java
@@ -0,0 +1,18 @@
+public class data {
+    
+	public static void main(String[] args) {
+		//Decalration
+		String day = "Monday";  
+		int date = 14; 
+        String month= "September"; 
+        int year = 2026; 
+        
+	System.out.print(day); 
+	System.out.print(", "); 
+	System.out.print(month + " "); 
+	System.out.print(date + ", "); 
+	System.out.print(year);  
+	
+	}
+
+}
