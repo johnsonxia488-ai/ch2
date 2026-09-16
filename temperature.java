c3bbf74ce1132a1a3d054bbf7ff00874f8e7f81b
johnsonxia488-ai <johnsonxia488@gmail.com>
2026-09-16 13:24:09 -0700

    Add files via upload

---
+++ temperature.java
@@ -0,0 +1,16 @@
+import java.util.Scanner; 
+
+
+public class temperature {
+	
+	public static void main(String[] args) {
+		final double CELCIUS_PER_FAHRENHEIT = 9.0 / 5; 
+		Scanner in = new Scanner(System.in); 
+		System.out.print("Enter in a celcius degree:  "); 
+		double inputValue = in.nextDouble(); 
+		double Celcius    = inputValue * CELCIUS_PER_FAHRENHEIT + 32; 
+		
+		System.out.print(Celcius);
+		
+	}
+	}
