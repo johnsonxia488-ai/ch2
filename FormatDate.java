b72270b4e6e2304826e291a56b47503827b4e39c
johnsonxia488-ai <johnsonxia488@gmail.com>
2026-09-23 12:23:52 -0700

    Add files via upload

--- FormatDate.java
+++ FormatDate.java
@@ -1,21 +1,16 @@
 public class FormatDate {
 	
 	public static void printAmerican(String day, int date, String month, int year) {
-		System.out.println(day + ", " + month + date + ", " + year); 
+		System.out.println(day + ", " + month + " " + date + ", " + year); 
 	}
 	
 	public static void printEuropean(String day, int date, String month, int year) { 
-		System.out.println(day + " " + date + month + " " + year); 
+		System.out.println(day + " " + date + " " + month + " " + year); 
 	}
 	
 	public static void main(String[] args) {
 		printAmerican("Wednesday", 22, "September", 2026); 
 
 		printEuropean("Wednesday", 22, "September", 2026); 
-	
+	}
 }
-
-
-
-
-
